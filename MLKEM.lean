8e2d05a592713850c0423f0395c57c29f7ea7b32
Hasan Yavuz <hasan.yavuz@live.de>
2025-11-18 23:17:48 +0100

    Add definitions and imports for MLKEM1024

--- MLKEM.lean
+++ MLKEM.lean
@@ -1,6 +1,21 @@
-import Mathlib
+import Mathlib.Algebra.BigOperators.Basic
+import Mathlib.Data.Finmap
+import Mathlib.Data.ZMod.Basic
 
--- ML-KEM-1024 (FIPS 203) – IND-CCA2-Sicherheit
-theorem ML_KEM_1024_IND_CCA2 : True := by trivial
+namespace MLKEM1024
 
-#check ML_KEM_1024_IND_CCA2
+def k := 4
+def η1 := 2
+def η2 := 2
+def du := 10
+def dv := 4
+
+def q : ℤ := 3329
+instance : Fact (Nat.Prime 3329) := sorry -- wird später bewiesen
+
+def n := 256
+
+abbrev R := Polynomial (ZMod q)
+abbrev V := Fin k → R
+
+end MLKEM1024
