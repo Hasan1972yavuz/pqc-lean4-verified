0ee3c6e9953f6c3ea913764aa84400b749f51bb3
Hasan Yavuz <hasan.yavuz@live.de>
2025-11-24 10:44:05 +0100

    Create Lakefile.lean

---
+++ Lakefile.lean
@@ -0,0 +1,8 @@
+import Lake
+open Lake DSL
+
+package «pqc-lean4-verified» where
+  -- add package configuration options here
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
