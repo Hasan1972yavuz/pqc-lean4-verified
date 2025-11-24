0ee7112ce3c211fd58a404afdb3872a2cada6138
Hasan Yavuz <hasan.yavuz@live.de>
2025-11-24 11:04:56 +0100

    Update Lakefile.lean

--- Lakefile.lean
+++ Lakefile.lean
@@ -2,7 +2,6 @@ import Lake
 open Lake DSL
 
 package pqc_lean4_verified where
-  -- Settings können leer bleiben
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
